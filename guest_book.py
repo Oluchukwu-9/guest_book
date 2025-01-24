5e8ee9a2e633d4463076131d116735a6a51f4f09
oluchukwu <victorogonwa@gmail.com>
2025-01-24 18:45:25 +0100

    Completed the exercise as stated in 10-5 of python crash course

---
+++ guest_book.py
@@ -0,0 +1,15 @@
+"""A program that prompts users for their name and stores all of it in 
+a guest_book.txt file.
+"""
+
+from pathlib import Path
+
+name = ''
+while name != 'quit':
+    user_name = input("Enter your name here (to exit enter (q)uit): ").lower()
+    name += f"Name: {user_name.strip()}\n"
+    if user_name == 'quit' or user_name == 'q':
+        break
+
+    path = Path('guest_book.txt')
+    path.write_text(name)
